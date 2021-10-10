14db177beaef6d750cd7cbcdabffc1fd889ed52c
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2021-10-10 21:16:44 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -42,7 +42,7 @@ Here you can search files in Inline mode as well as PM, Use the below buttons to
 START_MSG = environ.get('START_MSG', default_start_msg)
 
 FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
-OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
+OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=69cb2c1d")
 if FILE_CAPTION.strip() == "":
     CUSTOM_FILE_CAPTION=None
 else:
